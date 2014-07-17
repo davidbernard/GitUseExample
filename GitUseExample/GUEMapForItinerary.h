0e2f3a057d4ebb73c0eccef4aaaf1b90beae33e4
David Bernard <davidbernard@optushome.com.au>
2014-07-18 09:33:35 +1000

    Create new classes for map itinerary feature.

---
+++ GitUseExample/GUEMapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  GUEMapForItinerary.h
+//  GitUseExample
+//
+//  Created by David Bernard on 18/07/2014.
+//  Copyright (c) 2014 David Bernard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GUEMapForItinerary : NSObject
+
+@end
---
+++ GitUseExample/GUEMapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  GUEMapForItinerary.m
+//  GitUseExample
+//
+//  Created by David Bernard on 18/07/2014.
+//  Copyright (c) 2014 David Bernard. All rights reserved.
+//
+
+#import "GUEMapForItinerary.h"
+
+@implementation GUEMapForItinerary
+
+@end
